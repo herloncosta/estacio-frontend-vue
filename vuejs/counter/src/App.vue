b2fff11894572ac3b1fab571202da447b1ecc6f3
herloncosta <herlon36@gmail.com>
2024-05-02 12:06:11 -0300

    primeiro projeto: counter

---
+++ vuejs/counter/src/App.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { ref } from 'vue';
+
+let counter = ref(0);
+</script>
+
+<template>
+  <div class="container">
+    <div>
+      <h1>Vue Counter</h1>
+    </div>
+
+    <div>
+      <h2>{{ counter }}</h2>
+    </div>
+
+    <div class="buttons">
+      <button @click="counter--">-</button>
+      <button @click="counter++">+</button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-size: 20px;
+}
+
+.buttons {
+  display: flex;
+  justify-content: center;
+  gap: 10px;
+}
+
+.buttons button {
+  min-width: 80px;
+}
+</style>
