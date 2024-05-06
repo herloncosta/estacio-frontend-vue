b15a011a18c7498cdb92eaac036a22bbbda010f2
herloncosta <herlon36@gmail.com>
2024-05-05 21:15:23 -0300

    primeira atividade com vue

---
+++ vuejs/carteirinha-estudante/src/App.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { ref } from 'vue';
+import Carteirinha from './components/Carteirinha.vue';
+
+let nome = ref("")
+let matricula = ref("")
+</script>
+
+<template>
+  <main class="h-screen flex items-center justify-center">
+    <div class="w-[800px] mx-auto">
+      <div class="text-center text-3xl mt-10">
+        <h1>Gerador de Cateirinhas - Universidade XPTO</h1>
+      </div>
+
+      <section class="flex gap-6 justify-between mt-10">
+        <div class="form">
+          <form class="flex flex-col gap-3">
+            <div class="w-[300px] flex flex-col">
+              <label for="student-name" class="font-semibold">Name:</label>
+              <input id="student-name" v-model="nome" type="text" placeholder="digite seu nome..."
+                class="px-2 py-3 border-2 border-slate-500 rounded-md" maxlength="20">
+            </div>
+
+            <div class="w-[300px] flex flex-col">
+              <label for="student-registration" class="font-semibold">Matrícula:</label>
+              <input id="student-registration" v-model="matricula" type="text" placeholder="digite sua matrícula..."
+                class="px-2 py-3 border-2 border-slate-500 rounded-md" maxlength="12">
+            </div>
+          </form>
+        </div>
+
+        <Carteirinha :nome="nome" :matricula="matricula" />
+      </section>
+    </div>
+  </main>
+</template>
---
+++ vuejs/carteirinha-estudante/src/components/Carteirinha.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { defineProps } from 'vue';
+const { nome, matricula } = defineProps({ nome: String, matricula: String })
+</script>
+
+
+<template>
+  <section class="flexgap-4 border-4 min-w-[400px] min-h-[200px] border-slate-700 p-6 rounded-lg bg-cyan-400">
+    <div>
+      <h3 class="font-semibold">Universidade XPTO</h3>
+      <p>Identificação do Aluno</p>
+    </div>
+
+    <div class="flex items-center justify-between">
+      <div class="text-lg">
+        <p><span class="font-semibold">Nome:</span> {{ nome }}</p>
+        <p><span class="font-semibold">Matrícula:</span> {{ matricula }}</p>
+      </div>
+
+      <div class="size-[100px] rounded-full">
+        <img src="https://img.freepik.com/fotos-premium/desenho-de-menino-bonito_662214-8215.jpg"
+          alt="foto de perfil do aluno" class="rounded-full">
+      </div>
+    </div>
+  </section>
+</template>
